9e5782736b241c66afe4408eb01342b5599b44e5
DevDolphin7 <devdolphin7@outlook.com>
2026-08-06 12:34:07 +0100

    Separate IR Rx and Tx into different files, keep IRManager here

--- lib/managers/ir.py
+++ lib/managers/ir.py
@@ -1,9 +1,10 @@
-from time import sleep_ms, ticks_ms, ticks_diff
-from lib.core.event_bus import event_bus
 from lib.managers.ir_protocol import IRProtocol
+from lib.managers.reciever import IRReciever
+from lib.managers.transmitter import IRTransmitter
 from lib.managers.hardware import Buttons
 from lib.managers.device import device
-from lib.core.types import Events, Device, IRData
+from lib.core.event_bus import event_bus
+from lib.core.types import Events, Device
 
 
 class IRManager:
@@ -58,89 +59,3 @@ class IRManager:
     def set_up_from_device(self, device: Device) -> None:
         self.change_protocol()
         print(self._protocol.get_rx_name())
-
-
-class IRReciever:
-    def __init__(self, protocol: IRProtocol) -> None:
-        """Initializes the IR receiver with a given protocol and sets up the callback for received data."""
-        self.last_commands = []
-
-        self._protocol = protocol
-        self._received_index = 0
-        self._recieved_ticks = 0
-        self._last_receievd_ticks = 0
-
-        self._event_bus = event_bus
-
-        self.start()
-
-    def start(self) -> None:
-        self._protocol.set_rx(self.log_received_ir)
-
-    def log_received_ir(self, data, address, *control) -> None:
-        """Callback function to handle received IR data, printing it and storing it in the last_commands list."""
-        print(data, "<<<<<<")
-        if address is not None:
-            ticks = ticks_ms()
-            self._received_index += 1
-            ticks_since_last_packet = ticks_diff(ticks, self._recieved_ticks)
-            if ticks_since_last_packet < 250:
-                self.last_commands.append(
-                    {
-                        "index": self._received_index,
-                        "address": address,
-                        "command": data,
-                        "ticks diff": ticks_since_last_packet,
-                    }
-                )
-            else:
-                if len(self.last_commands) > 1:
-                    self.last_commands.pop(1)
-                self.last_commands.insert(
-                    0,
-                    IRData(
-                        address,
-                        data,
-                        self._protocol.get_rx_name(),
-                        self._received_index,
-                        ticks_diff(ticks_ms(), self._last_receievd_ticks),
-                    ),
-                )
-                self._last_receievd_ticks = ticks_ms()
-
-            if self._received_index > 255:
-                self._received_index = 0
-
-            self._event_bus.publish(Events.IR_RECEIVED, self.last_commands)
-
-    def close(self) -> None:
-        """Closes the IR receiver."""
-        self._protocol.rx.close()
-
-
-class IRTransmitter:
-    def __init__(self, address: int, protocol: IRProtocol) -> None:
-        """Initializes the IR transmitter with a given address and protocol."""
-        self._address = address
-        self._previous = 1
-        self._timeout = 500  # ms
-        self._timeout_interval = 20  # ms
-
-        self._protocol = protocol
-        self._protocol.set_tx()
-
-    def transmit_and_wait(self, address, command) -> None:
-        """Transmits an IR command and waits for the transmission to complete, raising a TimeoutError if it takes too long."""
-        self._protocol.tx.transmit(address, command)
-
-        time_elapsed = 0
-        while self._protocol.tx.busy() and time_elapsed < self._timeout:
-            sleep_ms(self._timeout_interval)
-            time_elapsed += self._timeout_interval
-
-            if time_elapsed > self._timeout:
-                raise TimeoutError("Transmission timeout")
-
-    def send_hex_command(self, command: int) -> None:
-        """Sends a hexadecimal IR command"""
-        self.transmit_and_wait(self._address, command)
