57b070d887495548335add2be84a63c6495dc2cf
DevDolphin7 <devdolphin7@outlook.com>
2026-09-04 23:00:05 +0100

    Mock EPD as a pytest hook prior to import

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import sys, types
+import pytest
+from unittest.mock import MagicMock
+
+
+@pytest.fixture(autouse=True)
+def reset_mocks():
+    """Reset the mocks before each test."""
+    yield
+    sys.modules[
+        "universal_remote.lib.drivers.epd1in54_V2"
+    ].EPD.frame_buffer.reset_mock()
+
+
+def pytest_configure():
+    fake_driver = types.ModuleType("universal_remote.lib.drivers.epd1in54_V2")
+
+    class MockEPD:
+        frame_buffer = MagicMock()
+
+    fake_driver.EPD = MockEPD
+
+    sys.modules["universal_remote.lib.drivers.epd1in54_V2"] = fake_driver
