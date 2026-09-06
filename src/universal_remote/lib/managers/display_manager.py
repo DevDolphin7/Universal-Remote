160a9de281f2c8eb85336e0422875898f3536586
DevDolphin7 <devdolphin7@outlook.com>
2026-09-06 22:17:26 +0100

    Current TDD progress

---
+++ src/universal_remote/lib/managers/display_manager.py
@@ -0,0 +1,125 @@
+from universal_remote.lib.core.event_bus import EventBus
+from universal_remote.lib.display.screen_normal import NormalScreen
+from universal_remote.lib.display.screen_learning import LearnScreen
+from universal_remote.lib.core.types import (
+    AppState,
+    Events,
+    AllButtons,
+    ProgrammableButtons,
+    Device,
+)
+
+
+class Display:
+    def __init__(
+        self,
+        event_bus: EventBus,
+        state: str = AppState.NORMAL,
+        remote_names: list[str] = [],
+        protocol_name: str = "",
+        battery_charge: int = 0,
+    ) -> None:
+        self._remote_index = 0
+        self._learning_remote = ""
+
+        self.set_protocol_name(protocol_name)
+        self.set_remote_names(remote_names)
+        self.set_battery_charge(battery_charge)
+        self.set_buttons()
+        self.set_button_index(self._buttons[0])
+        self.set_screen(state)
+
+        self._event_bus = event_bus
+        self._event_bus.subscribe(Events.BUTTON_RELEASED, self.handle_button_release)
+        self._event_bus.subscribe(Events.STATE_CHANGED, self.handle_state_change)
+        self._event_bus.subscribe(
+            Events.IR_PROTOCOL_CHANGED, self.handle_ir_protocol_change
+        )
+
+    def get_button(self) -> str:
+        return self._buttons[self._button_index]
+
+    def set_protocol_name(self, protocol_name: str) -> None:
+        self._protocol_name = protocol_name
+
+    def set_remote_names(self, remote_names: list[str]) -> None:
+        self._remote_names = remote_names
+
+    def set_screen(self, state: str) -> None:
+        if state == AppState.NORMAL:
+            self._screen = NormalScreen(
+                menu_items=self._remote_names, protocol_name=self._protocol_name
+            )
+        elif state == AppState.LEARNING:
+            self._screen = LearnScreen(
+                remote=self._learning_remote,
+                buttons=self._buttons,
+                protocol_name=self._protocol_name,
+            )
+
+    def set_battery_charge(self, charge: int) -> None:
+        self._battery_charge = charge
+
+    def set_buttons(self) -> None:
+        self._buttons = [
+            button for button in dir(ProgrammableButtons) if button[:2] != "__"
+        ]
+
+    def set_button_index(self, button: str) -> None:
+        self._button_index = self._buttons.index(button)
+
+    def scroll_menu(self) -> None:
+        if not isinstance(self._screen, NormalScreen):
+            return
+
+        if self._remote_index == len(self._screen.menu_items) - 1:
+            self._remote_index = 0
+        else:
+            self._remote_index += 1
+
+        self.update()
+
+    def update(self, learned: Device | None = None) -> None:
+        if isinstance(self._screen, NormalScreen):
+            self._screen.build(
+                selected_remote=self._remote_index, battery_charge=self._battery_charge
+            )
+        elif isinstance(self._screen, LearnScreen) and not learned:
+            self._screen.build_learning(
+                selected_button=self._button_index, battery_charge=self._battery_charge
+            )
+        elif isinstance(self._screen, LearnScreen) and learned:
+            button = self.get_button()
+
+            self._screen.build_learned(
+                selected_button=self._button_index,
+                id=learned.id,
+                address=learned.address,
+                press_command=learned.commands[button].press,
+                release_command=learned.commands[button].release,
+                battery_charge=self._battery_charge,
+            )
+
+        self._screen.update()
+
+    def handle_button_release(self, button: str, *args, **kwargs) -> None:
+        if button == AllButtons.MODE and isinstance(self._screen, NormalScreen):
+            self.scroll_menu()
+        elif button in dir(ProgrammableButtons) and isinstance(
+            self._screen, LearnScreen
+        ):
+            self.set_button_index(button)
+            self.update()
+
+    def handle_state_change(self, state: str, *args, **kwargs) -> None:
+        if state == AppState.LEARNING and isinstance(self._screen, NormalScreen):
+            self._learning_remote = self._screen.menu_items[self._remote_index]
+            self.set_screen(state)
+            self.update()
+        elif state == AppState.LEARNED and isinstance(self._screen, LearnScreen):
+            self.update(learned=args[0])
+
+    def handle_ir_protocol_change(self, protocol: str, *args, **kwargs) -> None:
+        if isinstance(self._screen, LearnScreen):
+            self._screen.set_protocol_name(protocol)
+            self.update()
--- tests/lib/managers/test_display_manager.py
+++ tests/lib/managers/test_display_manager.py
@@ -1,3 +1,16 @@
+import pytest, re
+from universal_remote.lib.core.event_bus import EventBus
+from universal_remote.lib.display.screen_normal import NormalScreen
+from universal_remote.lib.display.screen_learning import LearnScreen
+from universal_remote.lib.core.types import (
+    Events,
+    AllButtons,
+    AppState,
+    ProgrammableButtons,
+    IRProtocols,
+    Device,
+    Commands,
+)
 from universal_remote.lib.managers.display_manager import Display
 
 """
@@ -7,8 +20,8 @@ test_state_change_to_learning_changes_to_learn_screen
 
 LearnScreen
 test_programmable_button_released_displays_that_button_as_learning
-test_ir_received_displays_button_learned_information
-test_ir_protocol_changed_
+test_ir_protocol_changed_displays_new_protocol
+test_state_change_to_learned_displays_button_learned_information
 test_state_change_to_normal_changes_to_normal_screen
 
 AllScreens
@@ -17,5 +30,118 @@ test_low_battery_event_displays_empty_battery_icon
 """
 
 
+def assert_text_called_with_specific_text_once(frame_buffer, text: str):
+    text_call_args = [call[0][0] for call in frame_buffer.text.call_args_list]
+    arg_matches = re.findall(rf"{re.escape(text)}'", str(text_call_args))
+    print(text_call_args)
+    assert len(arg_matches) == 1
+
+
+@pytest.fixture
+def event_bus():
+    return EventBus()
+
+
+@pytest.fixture
+def remote_names():
+    return [f"Remote {i}" for i in range(8)]
+
+
 class TestDisplay:
-    pass
+    class TestNormalScreen:
+        def test_mode_button_released_cycles_active_remote(
+            self, event_bus, remote_names
+        ):
+            display = Display(event_bus, remote_names=remote_names, protocol_name="NEC")
+
+            assert isinstance(display._screen, NormalScreen)
+
+            event_bus.publish(Events.BUTTON_RELEASED, AllButtons.MODE)
+
+            assert isinstance(display._screen, NormalScreen)
+
+            with pytest.raises(AssertionError):
+                assert_text_called_with_specific_text_once(
+                    display._screen.frame_buffer, remote_names[0]
+                )
+
+            assert_text_called_with_specific_text_once(
+                display._screen.frame_buffer, remote_names[1]
+            )
+
+        def test_state_change_to_learning_changes_to_learn_screen(
+            self, event_bus, remote_names
+        ):
+            display = Display(event_bus, remote_names=remote_names, protocol_name="NEC")
+
+            assert isinstance(display._screen, NormalScreen)
+
+            event_bus.publish(Events.STATE_CHANGED, AppState.LEARNING)
+
+            assert isinstance(display._screen, LearnScreen)
+
+            assert_text_called_with_specific_text_once(
+                display._screen.frame_buffer, "Learning: " + ProgrammableButtons.CH_DOWN
+            )
+
+    class TestLearnScreen:
+        def test_programmable_button_released_displays_that_button_as_learning(
+            self, event_bus, remote_names
+        ):
+            display = Display(
+                event_bus,
+                remote_names=remote_names,
+                protocol_name="NEC",
+                state=AppState.LEARNING,
+            )
+
+            assert isinstance(display._screen, LearnScreen)
+
+            event_bus.publish(Events.BUTTON_RELEASED, ProgrammableButtons.NAV_OK)
+
+            assert_text_called_with_specific_text_once(
+                display._screen.frame_buffer, "Learning: " + ProgrammableButtons.NAV_OK
+            )
+
+        def test_ir_protocol_changed_displays_new_protocol(
+            self, event_bus, remote_names
+        ):
+            display = Display(
+                event_bus,
+                remote_names=remote_names,
+                protocol_name="NEC",
+                state=AppState.LEARNING,
+            )
+
+            event_bus.publish(Events.IR_PROTOCOL_CHANGED, IRProtocols.RC6_M0)
+
+            assert_text_called_with_specific_text_once(
+                display._screen.frame_buffer, "Protocol: " + IRProtocols.RC6_M0
+            )
+
+        def test_state_change_to_learned_displays_button_learned_information(
+            self, event_bus, remote_names
+        ):
+            display = Display(
+                event_bus,
+                remote_names=remote_names,
+                protocol_name="NEC",
+                state=AppState.LEARNING,
+            )
+
+            learned_device = Device(
+                id=1,
+                name="Remote",
+                protocol_name="NEC",
+                address=2,
+                commands={"NAV_OK": Commands(press=3, release=4)},
+            )
+
+            event_bus.publish(Events.BUTTON_RELEASED, ProgrammableButtons.NAV_OK)
+            event_bus.publish(Events.STATE_CHANGED, AppState.LEARNED, learned_device)
+
+            assert_text_called_with_specific_text_once(
+                display._screen.frame_buffer, "Address: " + hex(learned_device.address)
+            )
+
+        # test_state_change_to_normal_changes_to_normal_screen
