cfd41473cb9695f07dbd202206fca1becc692dde
DevDolphin7 <devdolphin7@outlook.com>
2026-09-08 21:58:39 +0100

    Handle device added and battery events, TDD complete

--- tests/lib/managers/test_display_manager.py
+++ tests/lib/managers/test_display_manager.py
@@ -4,7 +4,6 @@ from universal_remote.lib.display.screen_normal import NormalScreen
 from universal_remote.lib.display.screen_learning import LearnScreen
 from universal_remote.lib.core.types import (
     Events,
-    AllButtons,
     AppState,
     ProgrammableButtons,
     IRProtocols,
@@ -12,22 +11,6 @@ from universal_remote.lib.core.types import (
     Commands,
 )
 from universal_remote.lib.managers.display_manager import Display
-
-"""
-NormalScreen
-test_mode_button_released_cycles_active_remote
-test_state_change_to_learning_changes_to_learn_screen
-
-LearnScreen
-test_programmable_button_released_displays_that_button_as_learning
-test_ir_protocol_changed_displays_new_protocol
-test_state_change_to_learned_displays_button_learned_information
-test_state_change_to_normal_changes_to_normal_screen
-
-AllScreens
-test_battery_level_changed_event_updates_battery_icon
-test_low_battery_event_displays_empty_battery_icon
-"""
 
 
 def assert_text_called_with_specific_text_once(frame_buffer, text: str):
@@ -65,7 +48,7 @@ class TestDisplay:
                 )
 
             assert_text_called_with_specific_text_once(
-                display._screen.frame_buffer, remote_names[1]
+                display._screen.frame_buffer, "Remote 1"
             )
 
         def test_state_change_to_learning_changes_to_learn_screen(
@@ -81,6 +64,27 @@ class TestDisplay:
 
             assert_text_called_with_specific_text_once(
                 display._screen.frame_buffer, "Learning: " + display.get_button()
+            )
+
+        def test_device_added_event_displays_device_in_normal_screen_menu(
+            self, event_bus, remote_names
+        ):
+            display = Display(event_bus, remote_names=remote_names, protocol_name="NEC")
+
+            learned_device = Device(
+                id=1,
+                name="Added Remote",
+                protocol_name="NEC",
+                address=2,
+                commands={"NAV_OK": Commands(press=3, release=4)},
+            )
+
+            mock_updated_devices = [learned_device]
+
+            event_bus.publish(Events.DEVICE_ADDED, learned_device, mock_updated_devices)
+
+            assert_text_called_with_specific_text_once(
+                display._screen.frame_buffer, "Added Remote"
             )
 
     class TestLearnScreen:
@@ -158,3 +162,33 @@ class TestDisplay:
             event_bus.publish(Events.STATE_CHANGED, AppState.NORMAL)
 
             assert isinstance(display._screen, NormalScreen)
+
+    class TestAllScreens:
+
+        def test_battery_level_changed_event_updates_battery_icon(
+            self, event_bus, remote_names
+        ):
+            display = Display(
+                event_bus,
+                remote_names=remote_names,
+                protocol_name="NEC",
+                battery_charge=3,
+            )
+
+            event_bus.publish(Events.BATTERY_LEVEL_CHANGED, 2)
+
+            assert display._battery_charge == 2
+
+        def test_low_battery_event_displays_empty_battery_icon(
+            self, event_bus, remote_names
+        ):
+            display = Display(
+                event_bus,
+                remote_names=remote_names,
+                protocol_name="NEC",
+                battery_charge=3,
+            )
+
+            event_bus.publish(Events.LOW_BATTERY)
+
+            assert display._battery_charge == 0
